b85e444695e7af190e83ad881acd5b47a710d878
Glitch (blaze-aftershave) <none>
2018-01-18 14:30:40 +0000

    🕍🍁 Checkpoint .env:639182/8 server.js:639182/76 package.json:639182/3 my-folder/links1.txt:639182/35 links1.txt:639182/37 links2.js:639182/6 links2.txt:639182/2 public/client.js:639182/20 commands/cool-file.js:639182/14 commands/info.js:639182/58 commands/weather.js:639182/76 commands/ping.js:639182/2 uniq_commands/cool-file.js:639182/70 uniq_commands/uniq_nric.js:639182/56 uniq_commands/pokemon.js:639182/59 uniq_commands/random_nric.js:639182/27

--- server.js
+++ server.js
@@ -37,3 +37,67 @@ var dreams = [
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+const fs = require('fs');
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+bot.commands = new Discord.Collection();
+bot.uniq_commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands');
+const uniq_commandsFiles = fs.readdirSync('./uniq_commands');
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    bot.commands.set(command.name, command);
+}
+
+
+for(const file of uniq_commandsFiles) {
+    const uniq_command = require(`./uniq_commands/${file}`);
+    bot.uniq_commands.set(uniq_command.name, uniq_command)
+}
+
+
+bot.on('message', msg => {
+    if (!msg.content.startsWith(process.env.MADE_WITH) && !msg.author.bot) {
+       var upcase = msg.content.toLowerCase()
+        if(upcase.includes('random_nric')) {
+            bot.uniq_commands.get('random_nric').execute(msg);
+        }
+        else if(upcase.includes('p.pokemon')) {
+            bot.uniq_commands.get('p.pokemon').execute(msg);
+        }
+        else if(upcase.includes('legit') && upcase.includes('is')) {
+            bot.uniq_commands.get('legit').execute(msg)
+        }
+        else if(upcase == "info") {
+            msg.send("Running on Heroku created by Wenyue\nNode version: v8.9.3\nnpm: v5.5.1");
+        }
+    };
+
+    if(msg.author.bot) return;
+
+    const args = msg.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+    
+    if (!bot.commands.has(command)) return;
+
+    try {
+        bot.commands.get(command).execute(msg, args);
+    }
+    catch (error) {
+        console.error(error);
+        msg.reply('there was an error trying to execute that command!');
+    }
+
+});
+
+
+bot.on('ready', () => {
+    console.log('Welcome my master...');
+
+    bot.user.setActivity("~Your Mum~");
+});
+
+bot.login(`${process.env.SECRET}`);
+
---
+++ uniq_commands/uniq_nric.js
@@ -0,0 +1,40 @@
+module.exports = {
+    name: 'legit',
+    description: 'Checks if ur IC is legit',
+    execute(msg) {
+        function is_valid_nric(n) {
+           var str1 = `${n}`
+           var for_s_t = "JZIHGFEDCBA"
+           var  for_f_g = "XWUTRQPNMLK"
+            var checksum = (+str1.charAt(1) * 2) + (+str1.charAt(2) * 7) + (+str1.charAt(3)* 6) + (+str1.charAt(4) * 5) + (+str1.charAt(5) * 4) + (+str1.charAt(6) * 3) + (+str1.charAt(7) * 2);
+            var check = `${str1.charAt(8)}`;   
+          
+            var check1 =  +for_s_t.indexOf(check);
+         
+            var check2 =  +for_f_g.indexOf(check);
+         
+            if(str1.charAt(0) == "G" || str1.charAt(0) == "T") {
+                checksum += 4;
+            }
+            
+            if((str1.charAt(0) == "T" || str1.charAt(0) == "S") && (checksum % 11) == check1) {
+                return true ;
+            }
+            
+            else if((str1.charAt(0) == "F" || str1.charAt(0) == "G") && (checksum % 11) ==  check2) {
+                return true ;
+            }
+            else {
+                return false ;
+            }
+        }
+        
+       var ic = msg.content.toUpperCase().split('/ +/')[1]
+        if(is_valid_nric(ic)) {
+            msg.reply("YES It is legit haha");
+        }
+        else {
+            msg.reply("NOO IT is not!!");
+        }
+    }
+};
