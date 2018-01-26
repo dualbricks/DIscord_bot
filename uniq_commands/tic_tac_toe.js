b8ce2ba4743eaead570e5a5a6e24a77d2b179c0c
Glitch (blaze-aftershave) <none>
2018-01-26 04:35:20 +0000

    🍎👨 Checkpoint uniq_commands/tic_tac_toe.js:639182/1246

--- uniq_commands/tic_tac_toe.js
+++ uniq_commands/tic_tac_toe.js
@@ -1,4 +1,20 @@
 module.exports = {
   name : 'tic_tac_toe',
   description : 'tic tac toe game ',
-  excute(msg
+  excute(msg) {
+    var play_grid = [1,2,3,4,5,6,7,8,9]
+    var players = 0;
+    if(players == 0) {
+      player1 = msg.author.id
+      players += 1
+    }
+    
+    if(players == 1 && msg.author.id != player1) {
+      player2 = msg.author.id
+      players += 1
+    }
+    
+    if(players == 2) {
+    fucntion is_winner?(player,symbol) {
+      if
+    
