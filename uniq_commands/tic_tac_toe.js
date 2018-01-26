3ad83a413413a6df24a81e5e79ae72a8b8dfb8b4
Glitch (blaze-aftershave) <none>
2018-01-26 04:57:18 +0000

    💺🙈 Checkpoint uniq_commands/tic_tac_toe.js:639182/1766

--- uniq_commands/tic_tac_toe.js
+++ uniq_commands/tic_tac_toe.js
@@ -15,12 +15,22 @@ module.exports = {
     }
     
     if(players == 2) {
-    fucntion is_winner?(symbol) {
-      var hori_1 = play.grid[0] + play.grid[1] + play.grid[2] 
-      var hori_2 = play.grid[3] + play.grid[4] + play.grid[5]
-      var hori_3 = play.grid[6] + play.grid[7] + play.grid[8]
-      var vert_1 = play.grid[0] + play.grid[3] + play.grid[6]
-      var vert_2 = play.grid[1] + play.grid[4] + play.grid[7]
-      var vert_3 = play.grid[2] + play.grid[5] + play.grid[8]
-      var diag_1 = play.grid[0] + play.grid[4] + play.grid[8]
-      var diag_2 = play.grid[2] + play.grid[4] + play.grid[0]
+      var turn = 1
+      function is_winner(symbol) {
+        var hori_1 = play.grid[0] + play.grid[1] + play.grid[2] 
+        var hori_2 = play.grid[3] + play.grid[4] + play.grid[5]
+        var hori_3 = play.grid[6] + play.grid[7] + play.grid[8]
+        var vert_1 = play.grid[0] + play.grid[3] + play.grid[6]
+        var vert_2 = play.grid[1] + play.grid[4] + play.grid[7]
+        var vert_3 = play.grid[2] + play.grid[5] + play.grid[8]
+        var diag_1 = play.grid[0] + play.grid[4] + play.grid[8]
+        var diag_2 = play.grid[2] + play.grid[4] + play.grid[6]
+        if( hori_1 == `${symbol * 3}` || hori_2 == `${symbol * 3}` || hori_3 == `${symbol * 3}` || 
+          vert_1 == `${symbol * 3}` || vert_2 == `${symbol * 3}`  || vert_3 == `${symbol * 3}` ||
+          diag_1 == `${symbol * 3}` || diag_2 == `${symbol * 3}`) {
+        return true
+        }
+      }
+      function main_game(player,turn) {
+        if(
+        
