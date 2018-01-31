712c0f6ab90ea467792012622c691dd1979f6654
Glitch (blaze-aftershave) <none>
2018-01-31 14:01:32 +0000

    🚘📟 Checkpoint uniq_commands/number_of_pics.js:639182/16 links1.txt:639182/1

--- uniq_commands/number_of_pics.js
+++ uniq_commands/number_of_pics.js
@@ -5,13 +5,13 @@ module.exports = {
   execute(msg) {
     const fs = require('fs')
     var user = msg.author
-    if(user.id = '315430130056953861'){
+    if(user.id =='315430130056953861'){
       var fileBuffer =  fs.readFileSync('./links1.txt');
       var  to_string = fileBuffer.toString();
       var  lines_num = to_string.split('\n').length;
       msg.reply(`You have ${lines_num} in your collection!\n Use add_pic command to add more !!`)
     }
-    if(user.id = '325657843997081600') {
+    else if(user.id == '325657843997081600') {
      var fileBuffer =  fs.readFileSync('./links2.txt');
      var to_string = fileBuffer.toString();
      var lines_num = to_string.split('\n').length;
