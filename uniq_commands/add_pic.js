0777aa9b3216e0b79d196bba77f0ef2569208f60
Glitch (blaze-aftershave) <none>
2018-01-31 06:41:07 +0000

    🐛🐆 Checkpoint uniq_commands/add_pic.js:639182/196

--- uniq_commands/add_pic.js
+++ uniq_commands/add_pic.js
@@ -5,14 +5,24 @@ module.exports = {
       const fs = require('fs');
       var link = msg.content.split(' ')[1]
       if(msg.author.id =='315430130056953861'){
-        fs.appendFileSync('./links1.txt', `${link}`,'utf8');
-        msg.reply("Your pic has been added to `${msg.author.id}'s` collections")
+        fs.appendFileSync('links1.txt', `${link}`,'utf8',
+          function(err) {        
+        if (err) throw err;
+        // if no error
+        console.log("Data is appended to file successfully.")
+        });
+        msg.reply(`Your pic has been added to ${msg.author.username}'s collections`)
         msg.delete('2000')
        }
       else if(msg.author.id == '325657843997081600'){
-        fs.appendFileSync('./links2.txt', `${link}`, 'utf8');
-        msg.reply("Your pic has been added to `${msg.author.id}'s` collections")
-        msg.delete
+        fs.appendFileSync('links2.txt', `${link}`, 'utf8',
+        function(err) {        
+        if (err) throw err;
+        // if no error
+        console.log("Data is appended to file successfully.")
+        });
+        msg.reply(`Your pic has been added to ${msg.author.username}'s collections`)
+        msg.delete('2000')
     }
   }
 }
