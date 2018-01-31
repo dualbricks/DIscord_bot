bfe92ef084529736502b29ca561b63c5e05253cc
Glitch (blaze-aftershave) <none>
2018-01-31 13:29:06 +0000

    😻🌱 Checkpoint uniq_commands/add_pic.js:639182/120 links1.txt:639182/10

--- uniq_commands/add_pic.js
+++ uniq_commands/add_pic.js
@@ -4,18 +4,18 @@ module.exports = {
     usage : 'add_pic link',
     execute(msg) {
       const fs = require('fs');
-      var link = msg.content.split(' ')[1]
-      if(msg.author.id =='xxxxxxx'){
-        fs.appendFileSync('links1.txt',link,'utf8');
+      var link = '\n'+ msg.content.split(' ')[1]
+      if(msg.author.id =='315430130056953861'){
+        fs.appendFileSync('links1.txt',link ,'utf8');
         console.log("Data is appended to file successfully.")
         msg.reply(`Your pic has been added to ${msg.author.username}'s collections`)
         msg.delete('2000')
        }
-      else if(msg.author.id == 'xxxxxx'){
-        fs.appendFileSync('links2.txt',link, 'utf8');
+      else if(msg.author.id == '325657843997081600'){
+        fs.appendFileSync('links2.txt',link , 'utf8');
         msg.reply(`Your pic has been added to ${msg.author.username}'s collections`)
         msg.delete('2000')
-    }
+      }
       else{
         msg.reply('You are not allowed to add new pic!!');
       }
