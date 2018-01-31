d5cce8e7a0b8994c8f6c17aa3933375eb787e744
Glitch (blaze-aftershave) <none>
2018-01-31 12:40:41 +0000

    🌕🍬 Checkpoint uniq_commands/help.js:639182/918

--- uniq_commands/help.js
+++ uniq_commands/help.js
@@ -3,4 +3,15 @@ module.exports = {
   description : 'Help',
   usage : 'i want help',
   execute(msg) {
+    const { commands } = message.client;
+    const data = []
+    var names = (commands.map(command => command.name).split(',');
+    for(i = 0, i < names.length, i++) {
+      command = commands.get(`names[i]
+    msg.channel.send(`**Name:** ${command.name}`)
+
+if (command.description) data.push(`**Description:** ${command.description}`);
+if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
+if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+
     
