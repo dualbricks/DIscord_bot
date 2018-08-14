5e59eb5348fffbe300c9a9c4a12d35c24e98f3ef
Glitch (blaze-aftershave) <none>
2018-08-14 14:36:37 +0000

    🍞🍃 Checkpoint ./uniq_commands/bus.js:902861/3096

--- uniq_commands/bus.js
+++ uniq_commands/bus.js
@@ -3,14 +3,25 @@ module.exports = {
   description : 'Bus',
   usage : 'i want help',
   execute(msg) {
-    var http = require('http');
-  var request = http.request('GET', '/api/fo', 
-    {
+   var request = require('request');
+
+  var options = {
+    url: 'http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=83139',
+    headers: {
+      'User-Agent': 'request',
       'accept': 'application/json', 
       'api-key': `${process.env.BUS_Key}` 
-    });
+    }
+  };
 
-  request.on('response', function (response) {});    
-  request.end();
+function callback(error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var info = JSON.parse(body);
+    console.log(info + " Stars");
   }
-};
+}
+
+request(options, callback);
+}
+  
+}
