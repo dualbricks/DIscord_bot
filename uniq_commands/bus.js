7b5a7eb138a1016e408718f2b1786df2ee95d79d
Glitch (blaze-aftershave) <none>
2018-08-15 14:21:42 +0000

    📰⛈ Checkpoint ./uniq_commands/bus.js:902861/399

--- uniq_commands/bus.js
+++ uniq_commands/bus.js
@@ -1,26 +1,30 @@
 module.exports = {
   name: 'bus',
   description : 'Bus',
-  usage : 'i want help',
+  usage : 'bus busstopcode busNo',
   execute(msg) {
-   var request = require('request');
-
-  var options = {
-    url: 'http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=65191',
-    headers: {
+    var busStop = msg.content.split(' ')[1]
+    var busNo = msg.content.split(' ')[2]
+    var request = require('request');
+    var options = {
+      url: 'http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=65191',
+      headers: {
       'accept': 'application/json', 
       'AccountKey': `${process.env.BUS_Key}`,
-    }
-  };
+      }
+    };
 
-function callback(error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var rawdata = body
-    var parsed = JSON.parse(rawdata)
-    return parsed
+    function callback(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var rawdata = body
+        var parsed = JSON.parse(rawdata)
+        return parsed
+      }
+    }
+   var bus_data = request(options, callback);
+   function busArrivalTime(buscode, busNo, bus_data) {
+      for(var i = 0; i < bus_data.length-1 
+   
+   }
   }
-}
- var bus_data = request(options, callback);
-  
-}
 }
