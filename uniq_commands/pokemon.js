dd745a4fa3514ce76e4355775339692ce93befe8
Glitch (blaze-aftershave) <none>
2021-05-04 01:41:43 +0000

    👨‍❤️‍💋‍👨🍍 Checkpoint ./uniq_commands/pokemon.js:902861/634

--- uniq_commands/pokemon.js
+++ uniq_commands/pokemon.js
@@ -3,19 +3,22 @@ module.exports = {
     description: 'pokemon bot',
     usage: 'pokemon!',
     execute(msg) {
-
+      const Discord = require('discord.js');
+      const pokemon_caught = new Discord.MessageEmbed();
+      var Pokedex = require('pokedex-promise-v2');
+      var P = new Pokedex();
       var random_poke = Math.floor(Math.random() * 248);
       
       P.getPokemonByName(`${random_poke}`)
-        .then(function(response,body) {
-        var data = body;
-        console.log(data.sprites.front_default);
+        .then(function(response) {
+        var img = response.sprites.front_default
+        console.log(img);
       })
         .catch(function(error) {
         console.log('There was an ERROR: ', error);
       });
-        
-      
+       
+      const file = new Discord.MessageAttachment(`sa`)
         
     }
 }
