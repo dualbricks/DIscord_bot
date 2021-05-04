1f518726d28c23720814f9d6d9d97c9d5d07511b
Glitch (blaze-aftershave) <none>
2021-05-04 02:15:14 +0000

    👟☃️ Checkpoint ./uniq_commands/pokemon.js:902861/1793

--- uniq_commands/pokemon.js
+++ uniq_commands/pokemon.js
@@ -11,12 +11,33 @@ module.exports = {
       
       P.getPokemonByName(`${random_poke}`)
         .then(function(response) {
+        var name = response.name;
         var img = response.sprites.front_default
         console.log(img);
         pokemon_caught.setImage(img)
           .setTitle("Pokemon caught")
-          .setFooter("Congrats");
+          .setFooter(`congrats you have caught ${name}`);
         msg.reply(pokemon_caught);
+        const fs = require('fs') //importing file save
+        var xpPath = './data-pokemon.JSON'
+        var xpRead = fs.readFileSync(xpPath);
+        var xpFile = JSON.parse(xpRead); //ready for use
+        var userId = msg.author.id //user id here
+        
+        if (!xpFile[userId]) { //this checks if data for the user has already been created
+        xpFile[userId] = {pokemons_caught:{} , img: {}, total_number: 0}; 
+        fs.writeFileSync(xpPath, JSON.stringify(xpFile, null, 2));
+        }  
+        
+        else {
+              var xppVar = Number(xpFile.total_number) + 1;
+              
+              var roleToGive = "Awesome Role"
+              xpFile[userId] = {xpp: xppVar, xppr: xpprVar, currentRole: roleToGive};
+              fs.writeFileSync(xpPath, JSON.stringify(xpFile, null, 2));
+          
+
+        }
                          
         
       })
