8173e8ee3cf90dc325348fd45b507eac1f80a959
Glitch (blaze-aftershave) <none>
2021-05-04 06:17:30 +0000

    🌈❤️ Checkpoint ./uniq_commands/details.js:902861/520

--- uniq_commands/details.js
+++ uniq_commands/details.js
@@ -10,15 +10,15 @@ module.exports = {
     var xpRead = fs.readFileSync(xpPath);
     var xpFile = JSON.parse(xpRead);
     var name = msg.author.id;
-    pokemon_details.setTitle("POKEMONS CAUGHT:").setDescription("catch them all")
-    console.log(xpFile[name]);
-    if (xpFile[name].pokemons_caught[1] == "a") {
-      console.log(xpFile[name].pokemons_caught[1]);
-    }
+    pokemon_details.setTitle(`POKEMONS CAUGHT: total ${xpFile[name].total_number}`).setDescription("catch them all")
+    console.log(xpFile[name].img_pokemon);
+    console.log(xpFile[name].img_pokemon[1]);
     
-    ``
+    
+    var string_names = String(xpFile[name].pokemons_caught[i])
+    
     for(var i = 0; i < xpFile[name].total_number -1;i++) {
-      pokemon_details.addField({name:String(xpFile[name].pokemons_caught[i]), value:xpFile[name].img_pokemon[i]});
+      pokemon_details.addField({name:string_names, value:xpFile[name].img_pokemon[i]});
     }
     
     msg.reply(pokemon_details)
