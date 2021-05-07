2ce7de820abb94e4afca09faf351d13bfa7064e3
Glitch (blaze-aftershave) <none>
2021-05-07 04:59:16 +0000

    🛩🌗 Checkpoint ./uniq_commands/await.js:902861/420

--- uniq_commands/await.js
+++ uniq_commands/await.js
@@ -4,8 +4,13 @@ module.exports = {
     usage:' collect',
     execute(msg) {
       const Discord = require("discord.js");
+      var playerName = "";
       let filter = x => !msg.author.bot && !msg.content.startsWith('!');
       let collector = new Discord.MessageCollector(msg.channel, filter);
+      msg.channel.send("Bot is collecting now");
+      collector.on('collect', msg => {
+        
+      });
       
     }
 }
