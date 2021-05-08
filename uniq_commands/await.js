9aa58ef1f4532e73216d03b76993f759ca75e895
Glitch (blaze-aftershave) <none>
2021-05-08 17:26:58 +0000

    🎠🦃 Checkpoint ./uniq_commands/await.js:902861/1709

--- uniq_commands/await.js
+++ uniq_commands/await.js
@@ -3,40 +3,13 @@ module.exports = {
   description: "collect",
   usage: " collect",
   execute(msg) {
-    var playerName = "";
-    var running = true;
-    var arrayhehe = [];
-    let filter = x => !msg.author.bot && msg.content.startsWith("$");
+    const filter = m => m.content.startWith('$');
     const collector = msg.channel.createMessageCollector(filter);
-    function comeOn() {
-      msg.channel.send("Bot is collecting now");
-      collector.on("collect", msg => {
-        console.log(msg.content + "collected");
-      });
-      collector.on("end", async (collectedItem, reason) => {
-        function isDone() {
-          if (playerName != null) {
-            return new Promise(resolve => {
-              resolve("resolved");
-            });
-          }
-        }
-        await isDone();
-        console.log(collectedItem.size);
-        console.log(playerName + " ended");
-        msg.channel.send("Collection has stopped by " + playerName);
-        console.log(collectedItem.array()[0].content);
-        var contentArray = [];
-        for (let i = 0; i < collectedItem.size - 1; i++) {
-          arrayhehe.push(collectedItem.array()[i].content);
-        }
-        console.log(contentArray);
-      });
-    }
     
-    comeOn();
-    
-    
+    collector.on('collect', m => {
+      console.log(m.content);
+      channel.send("collect")
+    })
     
   }
 };
