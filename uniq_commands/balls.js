c4920e155f4403e6d04523085556ede758acbfdb
Glitch (blaze-aftershave) <none>
2021-05-10 01:35:36 +0000

    🏈🦀 Checkpoint ./uniq_commands/balls.js:902861/2902

--- uniq_commands/balls.js
+++ uniq_commands/balls.js
@@ -3,22 +3,31 @@ module.exports = {
   description: "give you balls",
   usage: "!balls",
   execute(msg) {
+    const fs = require("fs");
     const Discord = require("discord.js");
     var Pokedex = require("pokedex-promise-v2");
     var P = new Pokedex();
+    var number = Math.floor(Math.random() * 4);
     const balls_got = new Discord.MessageEmbed();
     P.getItemCategoryByName("standard-balls")
       .then(function(response) {
         var random = Math.floor(Math.random() * 6);
         console.log(response.items);
-        P.getItemByName(response.items[random].name).then(function(response2) {
-          balls_got
-            .setTitle(`congrats you got ${response2.name}`)
-            .setImage(response2.sprites.default);
-          msg.reply(balls_got);
-        }).catch(function(error) {
-          console.log("an error has occurred.")
-        });
+        P.getItemByName(response.items[random].name)
+          .then(function(response2) {
+            balls_got
+              .setTitle(`congrats you got ${number} ${response2.name}`)
+              .setImage(response2.sprites.default);
+            var xpPath = "./data-pokemon.json";
+            var xpRead = fs.readFileSync(xpPath);
+            var xpFile = JSON.parse(xpRead); //ready for use
+            var userId = msg.author.id; //user id here
+
+            msg.reply(balls_got);
+          })
+          .catch(function(error) {
+            console.log("an error has occurred.");
+          });
       })
       .catch(function(error) {
         console.log("An error has occurred");
