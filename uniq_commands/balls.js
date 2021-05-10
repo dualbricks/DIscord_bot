16dbd56b20142e4848aab78079cfdb04c3fa4828
Glitch (blaze-aftershave) <none>
2021-05-10 01:00:47 +0000

    🎳🏪 Checkpoint ./uniq_commands/balls.js:902861/906

--- uniq_commands/balls.js
+++ uniq_commands/balls.js
@@ -3,11 +3,17 @@ module.exports = {
   description: "give you balls",
   usage: "!balls",
   execute(msg) {
+    const Discord = require('discord.js');
     var Pokedex = require("pokedex-promise-v2");
     var P = new Pokedex();
+    const balls_got = new Discord.MessageEmbed();
     P.getItemCategoryByName("standard-balls")
       .then(function(response) {
+      var random = Math.floor(Math.random() * 6);
       console.log(response.items);
+      P.getItemByName(response.items[random]).then(function(response2) {
+        balls_got.setTitle(`congrats you got ${response2.name}`).setImage(re)
+      }) 
     }).catch(function(error) {
       console.log('An error has occurred');
     })
