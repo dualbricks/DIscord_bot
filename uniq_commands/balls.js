28d91367820d44488d5f381f6a8c12363b573a7c
Glitch (blaze-aftershave) <none>
2021-05-13 23:53:48 +0000

    👨☔️ Checkpoint ./uniq_commands/balls.js:902861/254

--- uniq_commands/balls.js
+++ uniq_commands/balls.js
@@ -7,7 +7,7 @@ module.exports = {
     const Discord = require("discord.js");
     var Pokedex = require("pokedex-promise-v2");
     var P = new Pokedex();
-    var number = Math.floor(Math.random() * 4);
+    var number = Math.floor(Math.random() * 4) + 1;
     const balls_got = new Discord.MessageEmbed();
     P.getItemCategoryByName("standard-balls")
       .then(function(response) {
@@ -22,14 +22,16 @@ module.exports = {
             var xpRead = fs.readFileSync(xpPath);
             var xpFile = JSON.parse(xpRead); //ready for use
             var userId = msg.author.id; //user id here
-            if(!xpFile[userId] || xpFile[userId].balls == null) {
+            if(!xpFile[userId] && xpFile[userId].balls == null) {
               xpFile[userId] = {
                 balls : {[response2.name] : [number]}
               }
+              fs.writeFileSync(xpPath, JSON.stringify(xpFile, null, 2));
             }
             else {
               var value = {[response2.name] : [number]}
               xpFile[userId].balls.push(value)
+              fs.writeFileSync(xpPath, JSON.stringify(xpFile, null, 2));
             }
             msg.reply(balls_got);
           })
