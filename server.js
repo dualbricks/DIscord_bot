40614acf6059eac1a470581a30ca58d0b4fb270e
Glitch (blaze-aftershave) <none>
2021-05-14 03:43:45 +0000

    🎭🏍 Checkpoint ./server.js:902861/240

--- server.js
+++ server.js
@@ -46,8 +46,11 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 bot.commands = new Discord.Collection();
 bot.uniq_commands = new Discord.Collection();
+bot.cooldowns = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands');
 const uniq_commandsFiles = fs.readdirSync('./uniq_commands');
+const {cooldowns} = bot;
+if(!cooldowns.has(uniq_commands.name))
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     bot.commands.set(command.name, command);
